c301d64aed718cf587acbea43414f967ad259952
Guilherme Brito <gbdbrito@gmail.com>
2024-11-15 16:55:01 -0300

    Criação da página de importação.#4

--- apuracaoVotos/inicio/models.py
+++ apuracaoVotos/inicio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class VotosCandidatos(models.Model):
+    nome = models.CharField(max_length=255)
+    votos = models.IntegerField(default=0)
+    sessao = models.CharField(max_length=4)
+    zona = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f'{self.nome}'
--- apuracaoVotos/inicio/views.py
+++ apuracaoVotos/inicio/views.py
@@ -4,69 +4,78 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-
-locais = {
-    "TOD": "Todos os dados",
-    "CCN": "Centro Cultural Caxias do Sul",
-    "CMD": "Câmara Municipal de Caxias do Sul",
-    "EMP": "Escola Municipal Professor Abelardo",
-    "CED": "Colégio Estadual Caxias do Sul",
-    "CCD": "Centro de Convenções de Caxias do Sul",
-    "SDC": "Sesc Caxias do Sul",
-    "PDP": "Praça Dante Alighieri",
-    "CSD": "Centro de Saúde Caxias do Sul",
-    "PNU": "Parque Natural Caxias do Sul",
-    "FDC": "Faculdade Caxias do Sul",
-    "BMD": "Biblioteca Municipal Caxias do Sul",
-    "EMF": "Estádio Municipal Caxias do Sul",
-    "AMB": "Associação de Moradores Bairro Pioneiro",
-    "HMD": "Hospital Municipal Caxias do Sul",
-    "CCX": "Clube Caxias",
-    "SCX": "Shopping Caxias do Sul",
-    "USS": "Unidade de Saúde São José",
-    "IMN": "Igreja Matriz de Caxias do Sul",
-    "CEA": "Complexo Esportivo Alvorada",
-    "CEP": "Centro de Eventos de Caxias do Sul"
-}
+from .models import VotosCandidatos
 
 
 def home(request):
     request.session['locais'] = {}
     request.session['totais'] = 0
+    request.session['totais_abstencao'] = 0
     request.session['votos_apurados'] = 0
+    request.session['total_sessoes'] = 0
+    request.session['sessoes_importadas'] = 0
     request.session['zonas'] = {}
     request.session.modified = True
+    limpar_dados()
     return render(request, 'home.html')
 
 
 def menu(request):
-    return render(request, 'base.html')
+    dic = {'nome_eleicao': request.session['nomeEleicao']}
+    return render(request, 'base.html', dic)
 
 
 def urnas(request):
-    dic = {'estados': locais, 'totalVotos': request.session['totais'],
-           'votosApurados': request.session['votos_apurados'], 'zonas': request.session['configuracao'].keys()}
+    valor: float = calcula_percentual(request.session['totais'], request.session['votos_apurados'])
+    dic = {'totalVotos': request.session['totais'], 'abstencao': request.session['totais_abstencao'],
+           'votosApurados': request.session['votos_apurados'], 'zonas': request.session['configuracao'].keys(),
+           'nome_eleicao': request.session['nomeEleicao'], 'total_sessoes': request.session['total_sessoes'],
+           'sessoes_importadas': request.session['sessoes_importadas'],
+           'percentual_sessao': calcula_percentual(request.session['total_sessoes'],
+                                                   request.session['sessoes_importadas']),
+           'percentual_votos': f"{valor:.2f}"}
     return render(request, 'apuracao_urnas.html', dic)
 
 
 def candidatos(request):
-    return render(request, 'candidatos.html')
+
+    dic = {'candidatos': get_candidatos(request)}
+    return render(request, 'candidatos.html', dic)
 
 
 def get_sessoes(request):
-    ses = request.session['configuracao'][request.GET['zona'].strip()]['sessoes'].keys()
-    qtdZonas = 0
-    print(request.session['configuracao'][request.GET['zona'].strip()]['sessoes']['001A']['votos'])
-    if request.GET.get('zona') and request.GET.get('sessao'):
-        qtdZonas = request.session['configuracao'][request.GET['zona'].strip()]['sessoes'][request.GET['sessao'].strip()]['votos']
-    elif request.GET['zona']:
-        qtdZonas = request.session['configuracao'][request.GET['zona'].strip()]['total_votos']
+    try:
+        if (not request.GET.get('zona') and not request.GET.get('sessao')) or not request.GET.get('zona'):
+            return JsonResponse(
+                {"sessoes": [], 'qtdVotosApurados': request.session['votos_apurados'], 'qtdAbstencaoApurados': 0,
+                 'status': 200,
+                 'percentual': f"{calcula_percentual(request.session['totais'], request.session['votos_apurados']):.2f}"})
+        ses = request.session['configuracao'][request.GET['zona'].strip()]['sessoes'].keys()
+        qtdZonas = 0
+        qtd_abstencao = 0
+        percentual = 0
+        if request.GET.get('zona') and request.GET.get('sessao'):
+            qtdZonas = \
+                request.session['configuracao'][request.GET['zona'].strip()]['sessoes'][request.GET['sessao'].strip()][
+                    'votos']
+            qtd_abstencao = \
+                request.session['configuracao'][request.GET['zona'].strip()]['sessoes'][request.GET['sessao'].strip()][
+                    'abstencao']
+        elif request.GET['zona']:
+            qtdZonas = request.session['configuracao'][request.GET['zona'].strip()]['total_votos']
+            qtd_abstencao = request.session['configuracao'][request.GET['zona'].strip()]['abstencao']
 
-    sessoes = []
-    for i in ses:
-        sessoes.append(i)
+        sessoes = []
+        for i in ses:
+            sessoes.append(i)
 
-    return JsonResponse({"sessoes": sessoes, 'qtdVotosApurados': qtdZonas})
+        return JsonResponse({"sessoes": sessoes, 'qtdVotosApurados': qtdZonas, 'qtdAbstencaoApurados': qtd_abstencao,
+                             'status': 200,
+                             'percentual': f"{calcula_percentual(request.session['totais'], qtdZonas):.2f}"})
+    except KeyError as e:
+        erro = f"A chave '{e.args[0]}' não foi encontrada na apuração dos votos."
+        return JsonResponse(
+            {'status': 400, 'chave': erro, 'qtdVotosApurados': 0, 'qtdAbstencaoApurados': 0, 'sessoes': []})
 
 
 def post_importar(request):
@@ -107,21 +116,27 @@ def organiza_arquivo(arquivo, est):
     file_content = arquivo.read().decode('utf-8')  # Converte de bytes para string
     json_data = json.loads(file_content)  # Converte a string para um dicionário Python
 
+    print(json_data)
+
     for i in json_data:
+        est.session['sessoes_importadas'] += 1
         est.session['configuracao'][i['idZona']]['total_votos'] += i['votosValidos']
+        est.session['configuracao'][i['idZona']]['abstencao'] += (i['quantidadePresentes'] - i['votosValidos'])
         est.session['votos_apurados'] += i['votosValidos']
+        est.session['totais_abstencao'] += (i['quantidadePresentes'] - i['votosValidos'])
         est.session['configuracao'][i['idZona']]['sessoes'][i['idSecao']]['votos'] += i['votosValidos']
+        est.session['configuracao'][i['idZona']]['sessoes'][i['idSecao']]['abstencao'] += (
+                i['quantidadePresentes'] - i['votosValidos'])
+
+        for p in i['candidatos']:
+            candidatos = VotosCandidatos.objects.get_or_create(nome=p['nomeCandidato'],
+                                                               sessao=i['idSecao'],
+                                                               zona=i['idZona'])
+
+            candidatos[0].votos += p['quantidadeVotos']
+            candidatos[0].save()
 
     est.session.modified = True
-
-    # for locais in json_data:
-    #     if not est.session['locais'].get(locais['nomeLocal']):
-    #         est.session['locais'][locais['nomeLocal']] = {'total_votos': 0}
-    #
-    #     est.session['locais'][locais['nomeLocal']]['total_votos'] += locais['votosSessao']
-    #     est.session['totais'] += locais['votosSessao']
-    #
-    # est.session.modified = True
 
 
 def configuracao_sessao(request):
@@ -142,7 +157,7 @@ def organiza_configuracao_apuracao(dado, ent):
 
     retorno = {'sucesso': True,
                'mensagem': '',
-                'status': 200}
+               'status': 200}
     try:
         file_content = dado.read().decode('utf-8')
         json_data = json.loads(file_content)
@@ -152,15 +167,19 @@ def organiza_configuracao_apuracao(dado, ent):
         dic = {}
         for i in json_data['zonasEleitorais']:
             if not dic.get(i['idZona']):
-                dic[i['idZona']] = {'total_votos': 0, 'sessoes': {}}
+                dic[i['idZona']] = {'total_votos': 0, 'sessoes': {}, 'abstencao': 0}
 
             for p in i['secoes']:
+                ent.session['total_sessoes'] += 1
                 if not dic[i['idZona']]['sessoes'].get(p['idSecao']):
-                    dic[i['idZona']]['sessoes'][p['idSecao']] = {'quantidadeEleitores': p['quantidadeEleitores'], 'votos': 0}
+                    dic[i['idZona']]['sessoes'][p['idSecao']] = {'quantidadeEleitores': p['quantidadeEleitores'],
+                                                                 'votos': 0,
+                                                                 'abstencao': 0}
 
                     ent.session['totais'] += p['quantidadeEleitores']
 
         ent.session['configuracao'] = dic
+        ent.session['nomeEleicao'] = json_data['nomeEleicao']
 
         ent.session.modified = True
         retorno['mensagem'] = 'Arquivo processado com sucesso'
@@ -174,5 +193,56 @@ def organiza_configuracao_apuracao(dado, ent):
         retorno['sucesso'] = False
         retorno['mensagem'] = 'Ocorreu um erro processar arquivo'
         retorno['status'] = 400
+
+    return retorno
+
+
+def calcula_percentual(total, votos):
+    percentual = (votos * 100) / total
+
+    return float(percentual)
+
+
+def limpar_dados():
+    # Limpa todos os dados da tabela 'Usuario'
+    VotosCandidatos.objects.all().delete()
+
+
+def get_candidatos(est):
+    retorno = {
+        "totalVotosValidos": 0,
+        "percentualVotosValidos": 0,
+        "candidatos": [
+        ]
+    }
+    import sqlite3
+
+    # Conectar ao banco de dados SQLite
+    conn = sqlite3.connect('db.sqlite3')
+
+    # Criar um cursor
+    cursor = conn.cursor()
+
+    cursor.execute(f"""SELECT
+                    nome,
+                    SUM(votos),
+                    ROUND((SUM(votos) * 100.0) /{est.session['totais']} , 2) AS percentual
+                FROM
+                    inicio_votoscandidatos
+                GROUP BY
+                    nome;
+    """)
+
+    resultado = cursor.fetchall()
+    for i in resultado:
+        retorno['totalVotosValidos'] += i[1]
+        retorno['percentualVotosValidos'] = f"{(retorno['totalVotosValidos'] * 100)/est.session['totais']:.2f}"
+        retorno['candidatos'].append({
+            "nomeCandidato": i[0],
+            "quantidadeVotos": i[1],
+            "percentualVotos": i[2]
+        })
+
+    conn.close()
 
     return retorno
